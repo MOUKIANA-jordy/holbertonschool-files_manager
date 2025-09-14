bf694d1639398fc6f30ca39a09a0ba7f6389a32f
MOUKIANA-jordy <jordymoukiana@gmail.com>
2025-09-14 18:20:48 +0200

    test server

---
+++ controllers/AppController.js
@@ -0,0 +1,22 @@
+import redisClient from '../utils/redis.mjs';
+import dbClient from '../utils/db.mjs';
+
+class AppController {
+  static async getStatus(req, res) {
+    // Vérifie si Redis et DB sont connectés
+    const status = {
+      redis: redisClient.isAlive(),
+      db: dbClient.isAlive(),
+    };
+    return res.status(200).json(status);
+  }
+
+  static async getStats(req, res) {
+    const users = await dbClient.nbUsers();
+    const files = await dbClient.nbFiles();
+
+    return res.status(200).json({ users, files });
+  }
+}
+
+export default AppController;
