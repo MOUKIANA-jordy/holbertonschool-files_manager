8a39689eda1222dfd3abaadfeeed8b2513325dc6
MOUKIANA-jordy <jordymoukiana@gmail.com>
2025-09-14 19:15:07 +0200

    create auth

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,20 @@
 import express from 'express';
-import AppController from '../controllers/AppController';
-import UsersController from '../controllers/UsersController';
+import AppController from '../controllers/AppController.js';
+import UsersController from '../controllers/UsersController.js';
+import AuthController from '../controllers/AuthController.js';
 
 const router = express.Router();
 
+// App endpoints
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
 
-// Users
+// Users endpoints
 router.post('/users', UsersController.postNew);
+router.get('/users/me', UsersController.getMe);
+
+// Auth endpoints
+router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.getDisconnect);
 
 export default router;
